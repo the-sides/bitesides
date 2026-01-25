bb6dbdc7ab262f806d3a6aebcb48e5bf7bf04398
Jacob Sides <jacob.r.sides@gmail.com>
2026-01-24 22:11:09 -0500

    Remove Nitro?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -50,7 +49,6 @@ const config = defineConfig({
         },
       }
     }),
-    nitro(),
     viteReact(),
   ],
 })
