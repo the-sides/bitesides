108bdc0e4694f28bfad04dced6ea3f831695cd6e
Jacob Sides <jacob.r.sides@gmail.com>
2026-01-24 22:31:44 -0500

    Test no network req

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ComponentExample } from "@/components/component-example";
-import { getReviews } from "@/lib/notion";
+// import { getReviews } from "@/lib/notion";
 
 export const Route = createFileRoute("/")({
   component: App,
   loader: async () => {
     try {
-      const reviews = await getReviews();
+      // const reviews = await getReviews();
+      const reviews = await new Promise(function (res) { setTimeout(() => res([]), 4000) })
       return { reviews };
     } catch (error) {
-      console.error("Failed to fetch reviews:", error);
+      console.error("!!!!!!! Failed to fetch reviews:", error);
       return { reviews: [] };
     }
   },
