47c7136ae666058bde175ae04468527eaf3576de
Jacob Sides <jacob.r.sides@gmail.com>
2026-01-24 23:03:41 -0500

    IT WORKED, bring back notion query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ComponentExample } from "@/components/component-example";
-// import { getReviews } from "@/lib/notion";
+import { getReviews } from "@/lib/notion";
 
 export const Route = createFileRoute("/")({
   component: App,
   loader: async () => {
     try {
-      // const reviews = await getReviews();
-      console.log("Waiting...")
-      await new Promise(function (res) { setTimeout(() => res([]), 4000) })
-      console.log("Done waiting")
-      return { reviews: [] };
+      const reviews = await getReviews();
+      return { reviews };
     } catch (error) {
       console.error("!!!!!!! Failed to fetch reviews:", error);
       return { reviews: [] };
