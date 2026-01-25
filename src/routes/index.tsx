ebfc90c0798b0f9d3020f1e1f547c6f6547adb79
Jacob Sides <jacob.r.sides@gmail.com>
2026-01-24 22:08:38 -0500

    Build: remove server function such that getReviews can be called from loader during build time

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
 import { ComponentExample } from "@/components/component-example";
 import { getReviews } from "@/lib/notion";
-
-const fetchReviews = createServerFn({ method: "GET" }).handler(async () => {
-  return getReviews();
-});
 
 export const Route = createFileRoute("/")({
   component: App,
   loader: async () => {
-    const reviews = await fetchReviews();
+    const reviews = await getReviews();
     return { reviews };
   },
-  // Enable static prerendering - data is fetched at build time
-  preload: true,
 });
 
 function App() {
